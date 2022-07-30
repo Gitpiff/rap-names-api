83a381cd47570addbaaddad16e40b68b443a94b7
Victor <viknavarro@gmail.com>
2022-07-30 18:44:49 -0500

    added heroku port

--- server.js
+++ server.js
@@ -39,6 +39,6 @@ app.get("/api/:rapper", (request, response) => {
     }
 })
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
     console.log(`Server running on port ${PORT}! Betta Go Catch It!`)
 })
